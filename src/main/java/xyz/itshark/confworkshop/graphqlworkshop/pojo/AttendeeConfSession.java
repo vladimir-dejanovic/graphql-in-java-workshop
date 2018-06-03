35e6ac25aad536374bba82831f6bf7c21806cab4
Vladimir Dejanovic <ed.wong.iv@gmail.com>
2018-06-03 22:00:04 +0200

    small touch

--- src/main/java/xyz/itshark/confworkshop/graphqlworkshop/pojo/AttendeeConfSession.java
+++ src/main/java/xyz/itshark/confworkshop/graphqlworkshop/pojo/AttendeeConfSession.java
@@ -23,6 +23,7 @@ import lombok.Data;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
@@ -30,7 +31,7 @@ import javax.persistence.Id;
 public class AttendeeConfSession {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private Long attendeeId;
