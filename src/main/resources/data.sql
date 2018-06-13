972761559cf3fc24c38f97e15797e615ae2f421c
Vladimir Dejanovic <vladimir@itshark.xyz>
2018-06-13 09:44:29 +0200

    improved data sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,28 +1,34 @@
-insert into SPEAKER
-values(1,'zika','zika je mali decak','@zika');
+insert into SPEAKER values(1,'speaker 1','speaker 1 info','@speaker1');
+insert into SPEAKER values(2,'speaker 2','speaker 2 info','@speaker2');
+insert into SPEAKER values(3,'speaker 3','speaker 3 info','@speaker3');
+insert into SPEAKER values(4,'speaker 4','speaker 4 info','@speaker4');
+insert into SPEAKER values(5,'speaker 5','speaker 5 info','@speaker5');
+insert into SPEAKER values(6,'speaker 6','speaker 6 info','@speaker6');
 
-insert into ATTENDEE
-values(1,'laza');
-insert into ATTENDEE
-values(2,'pera');
+insert into ATTENDEE values(1,'attendee 1');
+insert into ATTENDEE values(2,'attendee 2');
+insert into ATTENDEE values(3,'attendee 3');
+insert into ATTENDEE values(4,'attendee 4');
+insert into ATTENDEE values(5,'attendee 5');
+insert into ATTENDEE values(6,'attendee 6');
+insert into ATTENDEE values(7,'attendee 7');
+insert into ATTENDEE values(8,'attendee 8');
+insert into ATTENDEE values(9,'attendee 9');
+insert into ATTENDEE values(10,'attendee 10');
+insert into ATTENDEE values(11,'attendee 11');
+insert into ATTENDEE values(12,'attendee 12');
 
-insert into Conf_Session
-values (1,'graphql vs rest','let us look at graphql vs rest :)');
-insert into Conf_Session
-values (2,'conf2','this is conf session 2');
+insert into Conf_Session values (1,'graphql vs rest','let us look at graphql vs rest :)');
+insert into Conf_Session values (2,'conf 2','this is conf session 2');
+insert into Conf_Session values (3,'conf 3','this is conf session 3');
 
-insert into ATTENDEE_CONF_SESSION
-values(1,1,2);
+insert into ATTENDEE_CONF_SESSION values(1,1,2);
 
-insert into ATTENDEE_WORKSHOP
-values(1,1,1);
+insert into ATTENDEE_WORKSHOP values(1,1,1);
 
-insert into SPEAKER_CONF_SESSION
-values(1,1,1);
+insert into SPEAKER_CONF_SESSION values(1,1,1);
 
-insert into SPEAKER_WORKSHOP
-values(1,1,1);
+insert into SPEAKER_WORKSHOP values(1,1,1);
 
-insert into WORKSHOP
-values (1,'graphql in java world','let us build graphql java app','bring laptop');
+insert into WORKSHOP values (1,'graphql in java world','let us build graphql java app','bring laptop');
 
